83e3e5d02621b9e16a3ccc355262c52755e45196
Alex Yakovlev <leffalt@gmail.com>
2024-01-17 17:16:28 +0700

    add forms for authentication

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\SignInFormRequest;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function index()
+    {
+        return view('auth.index');
+    }
+
+    public function signUp()
+    {
+        return view('auth.sign-up');
+    }
+
+    public function signIn(SignInFormRequest $request)
+    {
+        $credentials = $request->validate([
+
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('dashboard');
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ])->onlyInput('email');
+    }
+}
